e98f61f243dc045e920547177a5d48089aebf2c3
k21971 <keith.simpson1971@gmail.com>
2022-05-03 16:38:25 +0000

    Small edit to inprogress display (shows 'beta' 2).

--- install-to-chroot.sh
+++ install-to-chroot.sh
@@ -51,8 +51,8 @@ set -e
 umask 022
 
 echo "Creating inprogress and extrainfo directories"
-mkdir -p "$NAO_CHROOT/dgldir/inprogress-gnoll41022"
-chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-gnoll41022"
+mkdir -p "$NAO_CHROOT/dgldir/inprogress-gnoll410b2"
+chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-gnoll410b2"
 mkdir -p "$NAO_CHROOT/dgldir/extrainfo-gnoll"
 chown "$USRGRP" "$NAO_CHROOT/dgldir/extrainfo-gnoll"
 
