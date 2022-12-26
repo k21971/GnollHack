36132407aa84d4ce3d14b8434837144a2485e233
k21971 <keith.simpson1971@gmail.com>
2022-12-26 02:13:52 +0000

    Update install script/hints file for new version 4.1.0.35 (beta 15).

--- install-to-chroot.sh
+++ install-to-chroot.sh
@@ -18,9 +18,9 @@ COMPRESSBIN="/bin/gzip"
 NH_GIT="/home/build/GnollHack"
 NH_BRANCH="master"
 # HACKDIR from include/config.h; aka nethack subdir inside chroot
-NHSUBDIR="gnollhack-4.1.0.34"
+NHSUBDIR="gnollhack-4.1.0.35"
 # VAR_PLAYGROUND from include/unixconf.h
-NH_VAR_PLAYGROUND="/gnollhack-4.1.0.34/var/"
+NH_VAR_PLAYGROUND="/gnollhack-4.1.0.35/var/"
 # END OF CONFIG
 ##############################################################################
 
@@ -45,8 +45,8 @@ set -e
 umask 022
 
 echo "Creating inprogress and extrainfo directories"
-mkdir -p "$NAO_CHROOT/dgldir/inprogress-gnoll410b14"
-chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-gnoll410b14"
+mkdir -p "$NAO_CHROOT/dgldir/inprogress-gnoll410b15"
+chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-gnoll410b15"
 mkdir -p "$NAO_CHROOT/dgldir/extrainfo-gnoll"
 chown "$USRGRP" "$NAO_CHROOT/dgldir/extrainfo-gnoll"
 
